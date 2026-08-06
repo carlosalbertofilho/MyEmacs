7e15ce4c057ae017dd3339e075d84221c48381e3
carlosalberto_filho <carlosalberto_filho@outlook.com>
2026-08-06 10:34:58 -0300

    fix(treesit): add nix store paths for emacs-treesit-grammars and home-manager-path

--- lisp/custom-lang.el
+++ lisp/custom-lang.el
@@ -14,10 +14,12 @@
   :custom
   (treesit-auto-install (unless noninteractive 'prompt))
   :config
-  ;; Registrar diretórios de gramáticas Tree-Sitter do Nix (NixOS / Home Manager)
-  (dolist (dir '("~/.nix-profile/lib"
-                 "/run/current-system/sw/lib"
-                 "~/.nix-profile/share/treesit-grammars"))
+  ;; Registrar diretórios de gramáticas Tree-Sitter do Nix (NixOS / Home Manager / Nix Store)
+  (dolist (dir (append (file-expand-wildcards "/nix/store/*-emacs-treesit-grammars")
+                       (file-expand-wildcards "/nix/store/*-home-manager-path")
+                       '("~/.nix-profile"
+                         "~/.nix-profile/lib"
+                         "/run/current-system/sw/lib")))
     (let ((expanded (expand-file-name dir)))
       (when (file-directory-p expanded)
         (add-to-list 'treesit-extra-load-path expanded))))
